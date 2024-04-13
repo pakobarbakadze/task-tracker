61f42630749242b229718b1611f7551a2a5761d2
pakobarbakadze <pakobarbakadze@gmail.com>
2024-04-13 15:26:29 +0400

    feat: header

---
+++ src/components/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }">{{ text }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: "black",
+    },
+  },
+  methods: {
+    onClick() {
+      console.log("Button clicked");
+    },
+  },
+});
+</script>
+
+<style scoped>
+button {
+  width: 50px;
+  height: 25px;
+  margin: auto 0px;
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tracker_header">
+    <h1>{{ title }}</h1>
+    <Button text="Add" color="green" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Button from "./Button.vue";
+
+export default defineComponent({
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  components: {
+    Button,
+  },
+});
+</script>
+
+<style scoped>
+.tracker_header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.tracker_header h1 {
+  font-size: 32px;
+}
+</style>
